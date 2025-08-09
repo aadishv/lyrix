c4adb7ee75379ac1f5b780402d4b1903d0e8690e
StarLight842 <842starlite@gmail.com>
2025-08-08 21:53:33 -0700

    feat: set up Sonner and add ShareLink component
    ShareLink copied from dotlite, will need to modify to remove password
    mode for use in sharing(!)

--- src/components/ProviderWrapper.tsx
+++ src/components/ProviderWrapper.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ export default function ProviderWrapper({ children }: { children: ReactNode }) {
         <NuqsAdapter>
           <QueryClientProvider client={queryClient}>
             {children}
+            <Toaster />
           </QueryClientProvider>
         </NuqsAdapter>
       </Authenticated>
